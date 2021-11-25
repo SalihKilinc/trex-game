ebdb1a7d32a541846a5166c11e4313f01df6de18
salih <askd@gmail.com@>
2021-11-25 16:09:42 +0100

    v3

---
+++ App.js
@@ -0,0 +1,33 @@
+ 
+const trex=document.querySelector("#trex");
+const kaktus=document.querySelector("#kaktus");
+ 
+function zipla(){
+   
+    if(kaktus.classList!="kaktus-animate")
+    {
+        kaktus.classList.add("kaktus-animate");
+    }
+ 
+    if(trex.classList!="trex-animate")
+    {
+        trex.classList.add("trex-animate");
+        setTimeout(function(){
+            trex.classList.remove("trex-animate");
+        },500);
+    }
+}
+var carpmaKontrol = setInterval (function(){
+    var trexBottom = parseInt(window. getComputedStyle(trex)
+    .getPropertyValue("bottom"));
+    
+    var kaktusLeft = parseInt(window. getComputedStyle(kaktus)
+    .getPropertyValue("left"));
+
+    if (kaktusLeft > 0 && kaktusLeft < 40 && trexBottom <40){
+        kaktus.classList.remove(kaktus-animate);
+        kaktus.style.display = "none";
+        alert("game over");
+    }
+}
+    ,10);
